1ba84e8798680bad3bf6c38bf36cdcfe44ce8598
anz186 <agamarmanda11@gmail.com>
2024-10-30 11:18:26 +0700

    mwehehe rusak njir

--- tjkt1-app/src/app/gallery/page.tsx
+++ tjkt1-app/src/app/gallery/page.tsx
@@ -17,41 +17,5 @@ export default function App() {
             <img className="h-auto max-w-full rounded-lg" src="./3.jpeg" alt="111"></img>
         </div>
     </div>
-    <div className="grid gap-4">
-        <div>
-            <img className="max-w-full rounded-lg" src="./4.jpeg" alt="1111"></img>
-        </div>
-        <div>
-            <img className="h-auto max-w-full rounded-lg" src="./5.jpeg" alt="11111"></img>
-        </div>
-        <div>
-            <img className="h-auto max-w-full rounded-lg" src="./6.jpeg" alt="111111"></img>
-        </div>
-    </div>
-    <div className="grid gap-4">
-        <div>
-            <img className="h-auto max-w-full rounded-lg" src="./11.jpeg" alt="1111111"></img>
-        </div>
-        <div>
-            <img className="h-auto max-w-full rounded-lg" src="./11.jpeg" alt="111111111"></img>
-        </div>
-        <div>
-            <img className="h-auto max-w-full rounded-lg" src="./11.jpeg" alt="111111111"></img>
-        </div>
-    </div>
-    <div className="grid gap-4">
-        <div>
-            <img className="h-auto max-w-full rounded-lg" src="./11.jpeg" alt="11"></img>
-        </div>
-        <div>
-            <img className="h-auto max-w-full rounded-lg" src="./12.jpeg" alt="11"></img>
-        </div>
-        <div>
-            <img className="h-auto max-w-full rounded-lg" src="./15.jpeg" alt="1"></img>
-        </div>
-    </div>
-</div>
-
-
   );
 }
